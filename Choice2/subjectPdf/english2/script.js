7e87dd80f91a0f9af2f06ae37dee10fb8b9d4f0f
Volvick <54volvick@gmail.com>
2025-03-08 08:13:16 +0530

    done all the changes i think its final

--- Choice2/subjectPdf/english2/script.js
+++ Choice2/subjectPdf/english2/script.js
@@ -6,8 +6,8 @@ const userAdmno = document.getElementById("admno");
 const userSession = document.getElementById("session");
 const userSubmit = document.getElementById("submitted");
 
-const generatePDF = async (name, classs, roll, admno,submitted,session) => {
-  const { PDFDocument, rgb } = PDFLib;
+const generatePDF = async (name, classs, roll, admno, submitted, session) => {
+  const { PDFDocument, rgb,degrees } = PDFLib;
 
   const capitalize = (str, lower = false) =>
     (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, (match) =>
@@ -26,48 +26,50 @@ const generatePDF = async (name, classs, roll, admno,submitted,session) => {
   const pages = pdfDoc.getPages();
   const firstPg = pages[0];
   firstPg.drawText(name, {
-    x: 74,
-    y: 246,
-    size: 14,
-    font: myFont,
-    color: rgb(0.078, 0.078, 0.078),
-  });
-  firstPg.drawText(classs, {
-    x: 76,
-    y: 197,
-    size: 14,
-    font: myFont,
-    color: rgb(0.078, 0.078, 0.078),
-  });
-  firstPg.drawText(roll, {
-    x: 76,
-    y: 154,
-    size: 14,
-    font: myFont,
-    color: rgb(0.078, 0.078, 0.078),
-  });
-  firstPg.drawText(admno, {
-    x: 76,
-    y: 103,
-    size: 14,
-    font: myFont,
-    color: rgb(0.078, 0.078, 0.078),
-  });
-
-  firstPg.drawText(session, {//submitted
-    x: 370,
-    y: 151,
-    size: 12,
-    font: myFont,
-    color: rgb(0.078, 0.078, 0.078),
-   
-  });
-  firstPg.drawText(submitted, {
-    x: 160,
-    y: 462,
-    size: 14,
-    font: myFont,
-    color: rgb(0.4, 0.404, 0.4),
+    x: 94,
+     y: 256,
+     size: 18,
+     font: myFont,
+     color: rgb(0.078, 0.078, 0.078),
+   });
+   firstPg.drawText(classs, {
+     x: 94,
+     y: 198,
+     size: 18,
+     font: myFont,
+     color: rgb(0.078, 0.078, 0.078),
+   });
+   firstPg.drawText(roll, {
+    x: 100,
+     y: 134,
+     size: 18,
+     font: myFont,
+     color: rgb(0.078, 0.078, 0.078),
+   });
+   firstPg.drawText(admno, {
+     x: 102,
+     y: 63,
+     size: 18,
+     font: myFont,
+     color: rgb(0.078, 0.078, 0.078),
+   });
+ 
+   firstPg.drawText(submitted, {
+    //session
+    x: 54,
+   y: 470,
+   size: 24,
+   font: myFont,
+   color: rgb(0.05, 0.05, 0.05),
+   rotate: degrees(90),
+ });
+   firstPg.drawText(session, {
+     //submitted
+       x: 344,
+       y: 114,
+       size: 15,
+       font: myFont,
+       color: rgb(0.05, 0.05, 0.05),
   });
   const uri = await pdfDoc.saveAsBase64({ dataUri: true });
   saveAs(uri, "PAGiCK.pdf", { autoBom: true });
@@ -90,13 +92,13 @@ submitBtn.addEventListener("click", () => {
   //check if the text is empty or not
   if (valName.trim() !== "" && userName.checkValidity()) {
     // console.log(val);
-    generatePDF(valName, valClass, valRoll, valAdmno, valSession,valSubmitted);
+    generatePDF(valName, valClass, valRoll, valAdmno, valSession, valSubmitted);
   } else if (valClass.trim() !== "" && userClasss.checkValidity()) {
-    generatePDF(valName, valClass, valRoll, valAdmno, valSession,valSubmitted);
+    generatePDF(valName, valClass, valRoll, valAdmno, valSession, valSubmitted);
   } else if (valRoll.trim() !== "" && userRoll.checkValidity()) {
-    generatePDF(valName, valClass, valRoll, valAdmno, valSession,valSubmitted);
+    generatePDF(valName, valClass, valRoll, valAdmno, valSession, valSubmitted);
   } else if (valAdmno.trim() !== "" && userAdmno.checkValidity()) {
-    generatePDF(valName, valClass, valRoll, valAdmno, valSession,valSubmitted);
+    generatePDF(valName, valClass, valRoll, valAdmno, valSession, valSubmitted);
   } else {
     userName.reportValidity();
   }
@@ -106,36 +108,3 @@ submitBtn.addEventListener("click", () => {
   saveAs(file);
 });
 
-// const generatePDF = async (name, classs, roll, admno) => {
-//   const { PDFDocument, rgb } = PDFLib;
-
-//   const exBytes = await fetch("./cert.pdf").then((res) => {
-//     return res.arrayBuffer();
-//   });
-
-//   const exFont = await fetch("./Poppins-Regular.ttf").then((res) => {
-//     return res.arrayBuffer();
-//   });
-
-//   const pdfDoc = await PDFDocument.load(exBytes);
-
-//   pdfDoc.registerFontkit(fontkit);
-//   const myFont = await pdfDoc.embedFont(exFont);
-
-//   const pages = pdfDoc.getPages();
-//   const firstPg = pages[0];
-
-//   firstPg.drawText(name, {
-//     x: 369,
-//     y: 151,
-//     size: 12,
-//     font: myFont,
-//     color: rgb(0.4, 0.404, 0.4),
-//   });
-
-//   const uri = await pdfDoc.saveAsBase64({ dataUri: true });
-
-//   document.querySelector("#mypdf").src = uri;
-// };
-
-// generatePDF("2024-2025");
